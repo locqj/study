089db5fb01adb2d5462196453f3f25fb6b39a7ed
locqj <locqj@163.com>
2017-07-29 09:26:53 +0800

    ->every day finish tag

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo 'start'
 time=`date '+%Y-%m-%d %H:%M:%S'`
-echo $time
-#date1=$(date +%Y-%m-%d %H%M%S)
 git add .
 git status
-git commit -m 'study tagl ${time}'
+git commit -m $date'->every day finish tag'
 git push
