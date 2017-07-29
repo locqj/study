6d385aa6035ba7951b3979094b356070cab0c453
locqj <locqj@163.com>
2017-07-29 09:11:28 +0800

    study tagl ${time}

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo 'start'
-date1=$(date +%Y-%m-%d %H%M%S)
+time=`date '+%Y-%m-%d %H:%M:%S'`
+echo $time
+#date1=$(date +%Y-%m-%d %H%M%S)
 git add .
 git status
-git commit -m $date1'study tagll'
+git commit -m 'study tagl ${time}'
 git push
